67b9581572def9bc8057c3a548b8410992ee87fb
Darshan Baral <darshanbaral@gmail.com>
2019-11-11 19:40:02 -0800

    Updates

---
+++ precache-manifest.fac61ac49515dfd6999431b42ea074a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4e61ed2f2d2361123960e443ba8a27",
+    "url": "/callbreak/index.html"
+  },
+  {
+    "revision": "f1cd235503c0dd52b792",
+    "url": "/callbreak/static/css/main.afb02f04.chunk.css"
+  },
+  {
+    "revision": "3eadadfc63de420e6292",
+    "url": "/callbreak/static/js/2.e02a39c7.chunk.js"
+  },
+  {
+    "revision": "f1cd235503c0dd52b792",
+    "url": "/callbreak/static/js/main.9386ea77.chunk.js"
+  },
+  {
+    "revision": "d166b6303e4967c29239",
+    "url": "/callbreak/static/js/runtime~main.a9318d4e.js"
+  }
+]);
