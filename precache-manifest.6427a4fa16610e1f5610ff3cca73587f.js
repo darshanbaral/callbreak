3fac6533a330600665047eacdda3296b56a70a6d
Darshan Baral <darshanbaral@gmail.com>
2019-11-12 21:00:27 -0800

    Updates

--- precache-manifest.6427a4fa16610e1f5610ff3cca73587f.js
+++ precache-manifest.6427a4fa16610e1f5610ff3cca73587f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b90abcb7c3ec4316521ea9ad0c09784e",
+    "revision": "2eaaa5682bc5a52a6d248347b14c0984",
     "url": "/callbreak/index.html"
   },
   {
-    "revision": "affa3b03c76c92a9a4cc",
+    "revision": "16c283c610f906ece812",
     "url": "/callbreak/static/css/main.afb02f04.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/callbreak/static/js/2.8b9c7c42.chunk.js"
   },
   {
-    "revision": "affa3b03c76c92a9a4cc",
-    "url": "/callbreak/static/js/main.675805fc.chunk.js"
+    "revision": "16c283c610f906ece812",
+    "url": "/callbreak/static/js/main.63286966.chunk.js"
   },
   {
     "revision": "d166b6303e4967c29239",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/callbreak/precache-manifest.314803fe27504bcb16596c619dcf8531.js"
+  "/callbreak/precache-manifest.6427a4fa16610e1f5610ff3cca73587f.js"
 );
 
 self.addEventListener('message', (event) => {
