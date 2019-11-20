012a9731e402e31d46b45247a4b9b5955bd3a36e
Darshan Baral <darshanbaral@gmail.com>
2019-11-19 19:42:49 -0800

    Updates

--- precache-manifest.0d82caa2ce967b4a09a67cd4c4fd59f1.js
+++ precache-manifest.0d82caa2ce967b4a09a67cd4c4fd59f1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "286f2d90f86e857c53bd2cd4337f30aa",
+    "revision": "847c9d3d2f79d3521e5f25c98fe64c12",
     "url": "/callbreak/index.html"
   },
   {
-    "revision": "66c94f1bdcd90c15603e",
+    "revision": "22c9301ffff416a7b524",
     "url": "/callbreak/static/css/main.4a4aa54b.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/callbreak/static/js/2.a1b4ee5f.chunk.js"
   },
   {
-    "revision": "66c94f1bdcd90c15603e",
-    "url": "/callbreak/static/js/main.5b5899b0.chunk.js"
+    "revision": "22c9301ffff416a7b524",
+    "url": "/callbreak/static/js/main.1e84f900.chunk.js"
   },
   {
     "revision": "d166b6303e4967c29239",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/callbreak/precache-manifest.6d2c90c1b894b2b347eae57ad861b76a.js"
+  "/callbreak/precache-manifest.0d82caa2ce967b4a09a67cd4c4fd59f1.js"
 );
 
 self.addEventListener('message', (event) => {
